99a79ff380832a47702e3f8adbad63ce7127e5d9
Gian Severiche <gianseveriche97@gmail.com>
2024-02-27 15:31:44 -0500

    Les hice algunas modificaciones al codigo para que sea mas facil de usar y de entender, no posee controles solo es el codigo a modo de ejercicio

--- funciones.py
+++ funciones.py
@@ -1,9 +1,5 @@
-"""
-En este archivo creare las funciones necesarias para el que la cuenta bancaria pueda cumplir con las ordenes y desiciones que el usuario decida hacer
-"""
-#_________________________________________________________________________________
 
-class persona():
+class Persona:
 
     """
     La clase persona se encargara de recibir el nombre y el apellido de los usuarios
@@ -12,43 +8,62 @@ class persona():
         self.nombre = nombre
         self.apellido = apellido
 #_________________________________________________________________________________
-class cliente(persona):
+class Cliente(Persona):
     """
     La clase cliente se encargara de llevar el numero de cuenta y el saldo ademas de heredar la clase de persona
     """
-    def __init__(self, numero_cuenta, saldo):
+    def __init__(self, nombre, apellido, numero_cuenta, saldo = 0):
+        super().__init__(nombre, apellido)
         self.numero_cuenta = numero_cuenta
         self.saldo = saldo
     
-    def imprimir(self):
-        return print(f"Usuario {self.nombre} {self.apellido}\nSu numero de cuenta es {self.numero_cuenta}\nSu saldo disponible es {self.saldo}")
+    def __str__ (self):
+        return f"Cliente: {self.nombre} {self.apellido}\nSaldo de cuenta: {self.numero_cuenta}: ${self.saldo}"
 
-    def depositar(self):
-        deposito = float(input("Por favor ingrese el la cantidad a depositar: "))
-        self.saldo = self.saldo + deposito
-        return print(f"Su nuevo saldo es {self.saldo}")
+    def depositar(self, deposito):
+        self.saldo += deposito
+        print("Deposito aceptado")
 
-    def retirar(self):
-        retiro = float(input("Por favor ingrese la cantidad a retirar: "))
-        if retiro < self.saldo:
-            print("Lo sentimos, saldo insuficiente")
+    def retirar(self, retiro):
+        if self.saldo <= retiro:
+            self.saldo -= retiro
+            print("Retiro exitoso")
         else:
-            self.saldo = self.saldo - retiro
-        return print(f"Su nuevo saldo es {self.saldo}")
+            print("Fondo insuficiente")
 #___________________________________________________________________________________
 
 def nuevo_usuario():
-    print("Creación de nuevo usuario\n")
-    nombre = str(input("Nombre de usuario: "))
-    apellido = str(input("Apellido de usuario: "))
-    return persona(nombre, apellido)
-
+    """
+    Esta funcion se encargara de crear a los nuevos usuarios y devolverlos como clientes
+    """
+    
+    print("Registro de nuevo usuario\n")
+    nombre_usuario = str(input("Nombre de usuario: "))
+    apellido_usuario = str(input("Apellido de usuario: "))
+    numero_cuenta = int(input("Ingrese su numero de cuenta: "))
+    cliente = Cliente(nombre_usuario, apellido_usuario, numero_cuenta)
+    return cliente
 #_________________________________________________________________________________
 def inicio():
     """
     Esta funcion da incio a todo el codigo manteniendo al usuario en un loop hasta que decida salir de la aplicación
     """
-    def nuevo_usuario():
-        pass
+    usuario = nuevo_usuario()
+    print(usuario)
+    opcion = 0
+
+    while opcion != 'S':
+        print("Elige una opcion:\nDepositar (D)\nRetirar (R)\nSalir (S)")
+        opcion = str.upper(input())
+
+        if opcion == 'D':
+            monto_deposito = int(input("Ingrese cantidad a depositar: "))
+            usuario.depositar(monto_deposito)
+        elif opcion == 'R':
+            monto_retiro = int(input("Ingrese cantidad a retirar: "))
+            usuario.retirar(monto_retiro)
+        print(usuario)
     
+    print("Gracias por operar con nuestro banco")
+
     
